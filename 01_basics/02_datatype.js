d5358a359d7dd03ce58be5bcf43f59d237ef2f8a
Anand jha <121583115+krish38506@users.noreply.github.com>
2024-01-16 10:22:36 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3) // we are using nodejs not browser
+// code readialblity should be high
+let name="anand"
+let age =18
+let islogged=false
+let state
+// number 2 to the power 53
+// bigint
+// string =""
+//boolen =>true/false
+// null =>standalone value
+// undefined=>
+// symbol=> unique
+
+
+// object
+console.log(typeof undefined)// undefiend
+console.log(typeof null)// object
