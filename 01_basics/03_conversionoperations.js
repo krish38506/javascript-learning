f8ca9dd15ad2e80d84ee4f492ba3fb38d3f88528
Anand jha <121583115+krish38506@users.noreply.github.com>
2024-01-18 13:04:42 +0000

    conversion on datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,21 @@
+//let score="33"
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valuenumber=Number(score)
+//console.log(typeof valuenumber)
+// "33"=>33
+// "33abc"=NaN not a number
+// true=>1: false=>0
+//let isloggedin=1
+//let boolenisloggedin=Boolean(isloggedin)
+//console.log(typeof boolenisloggedin)
+//console.log(boolenisloggedin)
+//1=> true: 0=>false
+// ""=>false
+//" anand"=>true
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber) // 33
+console.log(typeof stringnumber) // string
