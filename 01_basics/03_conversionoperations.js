d8079e25b25c37c7a3fcfd7eb8813b9360ffec70
Anand jha <121583115+krish38506@users.noreply.github.com>
2024-01-20 12:03:53 +0000

    comparison

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -13,7 +13,29 @@
 //1=> true: 0=>false
 // ""=>false
 //" anand"=>true
-let somenumber=33
-let stringnumber=String(somenumber)
-console.log(stringnumber) // 33
-console.log(typeof stringnumber) // string
+//let somenumber=33
+//let stringnumber=String(somenumber)
+//console.log(stringnumber) // 33
+//console.log(typeof stringnumber) // string
+
+//*********************************************operations******************************************************************
+//let value=3
+//let negvalue=-value
+//console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3); 2^3
+//console.log(2/3);
+//console.log(2%3);
+//let str1 = "hello"
+//let str2 = " anand"
+//let str3 = str1+str2// hello anand
+//console.log(str3)
+//console.log("1"+2)//12
+//console.log(1+ "2") //12
+//console.log("1"+2+2)// 122
+//console.log(1+2+"2") //32
+//console.log((3+4)*5 %3)
+//console.log(+true);
+//console.log(+"");
---
+++ 01_basics/comparison.js
@@ -0,0 +1,13 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2<1)
+//console.log(2<=1)
+
+// avoid below codes as we requried clean code
+//console.log("2">1)
+//console.log("02">1)
+//console.log(null >0)f
+//console.log(null==0) f
+//console.log(null>=0)t
+
+//console.log("2"===2) chek data type also
